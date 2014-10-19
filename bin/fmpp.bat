da2b00700d2ddb3946a8a18d76cf97d1622adb43
ddekany <ddekany@freemail.hu>
2014-10-19 10:59:31 +0200

    More licensing boilerplate fixes

--- bin/fmpp.bat
+++ bin/fmpp.bat
@@ -1,18 +1,27 @@
 @echo off
 
-rem  Copyright 2014 Attila Szegedi, Daniel Dekany, Jonathan Revusky
-rem 
-rem  Licensed under the Apache License, Version 2.0 (the "License");
-rem  you may not use this file except in compliance with the License.
-rem  You may obtain a copy of the License at
-rem 
-rem  http://www.apache.org/licenses/LICENSE-2.0
-rem 
-rem  Unless required by applicable law or agreed to in writing, software
-rem  distributed under the License is distributed on an "AS IS" BASIS,
-rem  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-rem  See the License for the specific language governing permissions and
-rem  limitations under the License.
+REM   Copyright 2014 Attila Szegedi, Daniel Dekany, Jonathan Revusky
+REM 
+REM   Licensed under the Apache License, Version 2.0 (the "License");
+REM   you may not use this file except in compliance with the License.
+REM   You may obtain a copy of the License at
+REM 
+REM   http://www.apache.org/licenses/LICENSE-2.0
+REM 
+REM   Unless required by applicable law or agreed to in writing, software
+REM   distributed under the License is distributed on an "AS IS" BASIS,
+REM   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+REM   See the License for the specific language governing permissions and
+REM   limitations under the License.
+REM
+REM   ------------------------------------------------------------------------
+REM
+REM   This file is a modified version of the file that comes with Ant 1.5.
+REM   The original version is copyrighted by The Apache Software Foundation.
+REM   The original copyright notice follows:
+REM
+REM   Copyright (c) 2001-2003 The Apache Software Foundation.  All rights
+REM   reserved.
 
 if exist "%HOME%\fmpprc_pre.bat" call "%HOME%\fmpprc_pre.bat"
 
--- bin/lcp.bat
+++ bin/lcp.bat
@@ -1,19 +1,25 @@
-rem  Copyright 2014 Attila Szegedi, Daniel Dekany, Jonathan Revusky
-rem 
-rem  Licensed under the Apache License, Version 2.0 (the "License");
-rem  you may not use this file except in compliance with the License.
-rem  You may obtain a copy of the License at
-rem 
-rem  http://www.apache.org/licenses/LICENSE-2.0
-rem 
-rem  Unless required by applicable law or agreed to in writing, software
-rem  distributed under the License is distributed on an "AS IS" BASIS,
-rem  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-rem  See the License for the specific language governing permissions and
-rem  limitations under the License.
-
-rem  This file is a modified version of the file comes with Ant 1.5.
-rem  The original version is copyrighted by The Apache Software Foundation.
+REM   Copyright 2014 Attila Szegedi, Daniel Dekany, Jonathan Revusky
+REM 
+REM   Licensed under the Apache License, Version 2.0 (the "License");
+REM   you may not use this file except in compliance with the License.
+REM   You may obtain a copy of the License at
+REM 
+REM   http://www.apache.org/licenses/LICENSE-2.0
+REM 
+REM   Unless required by applicable law or agreed to in writing, software
+REM   distributed under the License is distributed on an "AS IS" BASIS,
+REM   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+REM   See the License for the specific language governing permissions and
+REM   limitations under the License.
+REM
+REM   ------------------------------------------------------------------------
+REM
+REM   This file is a modified version of the file that comes with Ant 1.5.
+REM   The original version is copyrighted by The Apache Software Foundation.
+REM   The original copyright notice follows:
+REM
+REM   Copyright (c) 2001-2003 The Apache Software Foundation.  All rights
+REM   reserved.
 
 if "%FMPP_BAT_DEBUG%" == "on" echo [DEBUG] lcp.bat invoked with: %1
 
